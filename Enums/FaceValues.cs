549f6e21317e29615854be2313defb4879772f1a
dipakmahajan <35342979+dipakmahajan@users.noreply.github.com>
2018-02-19 11:34:33 -0500

    Add files via upload

---
+++ Enums/FaceValues.cs
@@ -0,0 +1,38 @@
+﻿namespace PlayingCards.Enums
+{
+    /// <summary>
+    /// Enumeration for face values in playing cards
+    /// </summary>
+    public enum FaceValues
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+
+        /// <summary>
+        /// Jack
+        /// </summary>
+        J = 11,
+
+        /// <summary>
+        /// Queen
+        /// </summary>
+        Q = 12,
+
+        /// <summary>
+        /// King
+        /// </summary>
+        K = 13,
+
+        /// <summary>
+        /// Ace
+        /// </summary>
+        A = 14
+    }
+}
---
+++ Enums/Suits.cs
@@ -0,0 +1,28 @@
+﻿namespace PlayingCards.Enums
+{
+    /// <summary>
+    /// Enumeration for suits in playing cards
+    /// </summary>
+    public enum Suits
+    {
+        /// <summary>
+        /// Spade
+        /// </summary>
+        S = 1,
+
+        /// <summary>
+        /// Heart
+        /// </summary>
+        H = 2,
+
+        /// <summary>
+        /// Diamond
+        /// </summary>
+        D = 3,
+
+        /// <summary>
+        /// Club
+        /// </summary>
+        C = 4
+    }
+}
